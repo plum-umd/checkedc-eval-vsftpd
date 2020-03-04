50a2be3040f323ee07c42a74253ce47eeb58663e
Michael Hicks <mwh@cs.umd.edu>
2020-03-04 12:53:27 -0500

    Added convert_all.sh to run porting tool on all files. Updated JSON file to be consistent

---
+++ convert_all.sh
@@ -0,0 +1,50 @@
+CCONV=/Users/mwhicks/research/projects/checkedc/checkedc-clang/llvm/cmake-build-debug/bin/CConvertStandalone
+INCLUDES=/Users/mwhicks/research/projects/checkedc/checkedc/include
+BASEDIR=.
+
+CPATH=$INCLUDES $CCONV \
+-dump-stats \
+-output-postfix=checked \
+-dump-intermediate \
+-extra-arg-before=-D_FORTIFY_SOURCE=2 \
+-extra-arg-before=-w \
+-base-dir="$BASEDIR" \
+$BASEDIR/ftpdataio.c \
+$BASEDIR/ascii.c \
+$BASEDIR/str.c \
+$BASEDIR/seccompsandbox.c \
+$BASEDIR/utility.c \
+$BASEDIR/twoprocess.c \
+$BASEDIR/ftpcmdio.c \
+$BASEDIR/filestr.c \
+$BASEDIR/postprivparent.c \
+$BASEDIR/access.c \
+$BASEDIR/netstr.c \
+$BASEDIR/strlist.c \
+$BASEDIR/ptracesandbox.c \
+$BASEDIR/privsock.c \
+$BASEDIR/ssl.c \
+$BASEDIR/sysutil.c \
+$BASEDIR/prelogin.c \
+$BASEDIR/opts.c \
+$BASEDIR/logging.c \
+$BASEDIR/readwrite.c \
+$BASEDIR/banner.c \
+$BASEDIR/hash.c \
+$BASEDIR/privops.c \
+$BASEDIR/ls.c \
+$BASEDIR/tunables.c \
+$BASEDIR/postlogin.c \
+$BASEDIR/secutil.c \
+$BASEDIR/secbuf.c \
+$BASEDIR/sslslave.c \
+$BASEDIR/ipaddrparse.c \
+$BASEDIR/oneprocess.c \
+$BASEDIR/features.c \
+$BASEDIR/standalone.c \
+$BASEDIR/sysstr.c \
+$BASEDIR/ftppolicy.c \
+$BASEDIR/main.c \
+$BASEDIR/parseconf.c \
+$BASEDIR/sysdeputil.c \
+$BASEDIR/tcpwrap.c

5466e5fbc520753cc0e4dc9be8339868844d66d1
Michael Hicks <mwh@cs.umd.edu>
2020-03-05 10:08:42 -0500

    Notes about vsftpd and conversion

---
+++ replace.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python3
+
+import re
+from os import listdir
+from os import system
+from os.path import isfile, join
+
+checkre = re.compile("\.checked")
+
+def ischecked(f):
+    return '.checked.c' in f or '.checked.h' in f
+
+checked = [f for f in listdir('.') if isfile(f) and ischecked(f)]
+
+for f in checked:
+    dest = re.sub(checkre, '', f)
+    system("mv %s %s" % (f, dest))
+
+print('Done')

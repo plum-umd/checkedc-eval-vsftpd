e7b58d0a62d546f2a09a7f6c250c0e146fccf6ff
John Kastner <john@correctcomputation.com>
2020-10-23 12:36:56 -0400

    user malloc

--- convert.sh
+++ convert.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
-cconv-standalone -dump-stats -alltypes --output-postfix=checked $(jq -r ".[] | .file" < compile_commands.json)
+cconv-standalone --use-malloc=vsf_sysutil_malloc -dump-stats -alltypes --output-postfix=checked $(jq -r ".[] | .file" < compile_commands.json)
 rename -f "s/.checked//" *.checked.c *.checked.h

94895cb076edcfb011cfcf55406e7508e8eab925
John Kastner <john@correctcomputation.com>
2020-11-13 12:24:01 -0500

    dump_intermediate

--- convert.sh
+++ convert.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
-cconv-standalone -dump-stats -alltypes --output-postfix=checked $(jq -r ".[] | .file" < compile_commands.json)
+3c -dump-intermediate -dump-stats -alltypes --output-postfix=checked $(jq -r ".[] | .file" < compile_commands.json)
 rename -f "s/.checked//" *.checked.c *.checked.h
